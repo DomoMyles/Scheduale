ba749647e8abde5aa3cea83721185fcaf45c4ea8
DomoMyles <47869842+DomoMyles@users.noreply.github.com>
2022-08-08 22:12:15 -0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+var today = new Date();
+var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+
+var time = today.getHours()
+console.log(time)
+
+if (time == 9) {
+    console.log(today)
+     console.log("Whaterman")
+}
+
+if (time == 10) {
+    console.log(today)
+     console.log("Whaterman")
+}
+
+if (time == 11) {
+    console.log(today)
+     console.log("Whaterman")
+}
+
+if (time == 12) {
+    console.log(today)
+     console.log("Whaterman")
+}
+
+if (time == 13) {
+    console.log(today)
+     console.log("Whaterman")
+}
+if (time == 14) {
+    console.log(today)
+     console.log("Whaterman")
+}
+
+if (time == 15) {
+    console.log(today)
+     console.log("Whaterman")
+}
+
+function resefdt(){
+    document.getElementsByClassName('pixel').style.backgroundColor = "white";
+}
